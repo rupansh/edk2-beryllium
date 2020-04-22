6e81620b4951eb20ac5d81113211963c5f1278e0
rupansh-arch <rupanshsekar@hotmail.com>
2020-04-23 04:11:54 +0530

    [USER-SPECIFIC] update gcc path
    Switched to arch from mac kek
    
    Signed-off-by: rupansh-arch <rupanshsekar@hotmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p beryllium/beryllium.dsc
+GCC5_AARCH64_PREFIX=$HOME/android/aarch64--glibc--stable-2020.02-2/bin/aarch64-linux- build -s -n 0 -a AARCH64 -t GCC5 -p beryllium/beryllium.dsc
 gzip -c < workspace/Build/beryllium/DEBUG_GCC5/FV/BERYLLIUM_UEFI.fd >uefi.img
 cat beryllium.dtb >>uefi.img
