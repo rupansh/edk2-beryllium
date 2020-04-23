0274e50fdd44f2a4f7227552d569b18083c69b3f
fxsheep <fxsheep@users.noreply.github.com>
2020-04-23 21:24:49 +0530

    wrap uefi as android boot image
    fuck fastboot bugs
    
    Signed-off-by: rupansh-arch <rupanshsekar@hotmail.com>

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_AARCH64_PREFIX=$HOME/android/aarch64--glibc--stable-2020.02-2/bin/aarch64-linux- build -s -n 0 -a AARCH64 -t GCC5 -p beryllium/beryllium.dsc
-gzip -c < workspace/Build/beryllium/DEBUG_GCC5/FV/BERYLLIUM_UEFI.fd >uefi.img
-cat beryllium.dtb >>uefi.img
+gzip -c < workspace/Build/beryllium/DEBUG_GCC5/FV/BERYLLIUM_UEFI.fd >uefi_image
+cat beryllium.dtb >>uefi_image
+abootimg --create uefi.img -k uefi_image -r ramdisk-null -f bootimg.cfg
